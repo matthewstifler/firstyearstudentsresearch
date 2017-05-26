ff503ba898aa76b843c36febf6f987b589c9c7c4
mishaulitskiy <mtwstflr@gmail.com>
2017-05-26 16:48:56 +0000

    Finished rewriting students-info gathering Removed not needed code

--- code/01-data-gathering.R
+++ code/01-data-gathering.R
@@ -58,5 +58,28 @@ sapply(students.data, function(y) { #take each person
 
 #turn list into an id-city data frame
 students.ids.cities.df <- sapply(students.data, function(x) {
-  if(length(x$schools) > 0) return(as.data.frame(list(id = y$uid, city = y$schools[[length(y$schools)]]$city), stringsAsFactors = FALSE))
+  if(length(x$schools) > 0) return(as.data.frame(list(user.id = y$uid, city = y$schools[[length(y$schools)]]$city), stringsAsFactors = FALSE))
 }) %>% rbind.fill()
+
+#adding demographic info to the existing students info
+students.ids.cities.df <- lapply(students.ids.cities.df$user.id, function(x){
+  Sys.sleep(1)
+  user.data <- content(GET(paste0("https://api.vk.com/method/", "users.get", 
+                                  "?access_token=", "24f3f52000d221fb9d47c9039134fb3623108c4cf67d24dae0b772702d1b9ab6750c220e3ff7989cd3a17", 
+                                  "&user_ids=", as.character(x),
+                                  "&fields=", "sex,bdate,country")), as = "parsed")$response
+  message(paste0("Processing user ", x))
+  return(data.frame(gender = ifelse(!is.null(user.data[[1]]$sex), user.data[[1]]$sex, NA),
+                    dob = ifelse(!is.null(user.data[[1]]$bdate), as.Date(user.data[[1]]$bdate, format = "%d.%m.%Y"), NA),
+                    country = ifelse(!is.null(user.data[[1]]$country), user.data[[1]]$country, NA)))
+}) %>%
+  rbind.fill %>%
+  cbind(students.ids.cities.df, .)
+
+students.ids.cities.df$is.local <- students.ids.cities.df$city == "2"
+table(students.ids.cities.df$is.local) / nrow(students.ids.cities.df)
+students.ids.cities.df$dob <- as.Date(students.ids.cities.df$dob, origin = "1970-01-01")
+students.ids.cities.df[,c("city", "gender", "country", "user.id")] <- lapply(students.ids.cities.df[,c("city", "gender", "country", "user.id")], as.factor)
+students.ids.cities.df$age <- ((as.Date(Sys.Date()) - students.ids.cities.df$dob) / 365.242) %>% as.numeric %>% round
+
+
